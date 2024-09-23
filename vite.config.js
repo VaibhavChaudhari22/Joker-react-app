583db6d362ff4f1af1dbc8371d50099806e28298
vaibhav chaudhari <chaudhari2208@gmail.com>
2024-09-23 19:35:05 +0530

    add port

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',  // Ensures the build output goes to the 'dist' folder
-  },
-});
+  preview: {
+    port: process.env.PORT || 4173, // Use the Render assigned port or default to 4173
+    host: '0.0.0.0', // Bind to all network interfaces
+  }
+})
